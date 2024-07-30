146dcba3c9ca94a075bfa436b9a5f1a157261dad
naquio <ignaciocanal@live.com.ar>
2024-07-30 20:41:11 -0300

    Botones y modo oscuro claro

--- js/app.js
+++ js/app.js
@@ -1,7 +1,11 @@
+import scrollTopButton from "./boton_scroll.js";
 import hamburgerMenu from "./menu_hamburger.js";
+import darkTheme from "./tema_oscuro.js";
 
 const d = document;
 
 d.addEventListener("DOMContentLoaded", (e) =>{
   hamburgerMenu(".boton",".panel",".menu a");
+  scrollTopButton(".scroll-top-btn");
+  darkTheme(".dark-theme-btn","dark-mode");
 });
